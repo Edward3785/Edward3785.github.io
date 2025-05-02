b8c1ebd1abcec9ace26947aa152ce01aab4e2d77
Edward3785 <emichael1@my.waketech.edu>
2025-05-02 19:47:57 -0400

    Update gradebook.js

--- public/gradebook.js
+++ public/gradebook.js
@@ -1,46 +1,49 @@
 function fetchGradeData() {
-	console.log("Fetching grade data...");
+	console.log("Fetching grade data. . .");
 
 	let xhr = new XMLHttpRequest();
+
 	let apiRoute = "/api/grades";
-
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			if (xhr.status !== 200) {
-				console.error(`Could not get grades. Status: ${xhr.status}`);
-				return;
+	
+	xhr.onreadystatechange = function(){
+		let results;
+		if(xhr.readyState === xhr.DONE){
+			if(xhr.status !== 200){
+				console.error('Could not get grades.
+					Status: $(xhr.status}');
 			}
-			const data = JSON.parse(xhr.responseText);
-			populateGradebook(data);
-		}
-	};
-
-	xhr.open("GET", apiRoute, true);
+			populateGradebook(JSON.parse(xhr.responseText));
+			}
+	}.bind(this);
+	xhr.open("get", apiRoute, true);
 	xhr.send();
 }
+
 
 function populateGradebook(data) {
 	console.log("Populating gradebook with data:", data);
 	let tableElm = document.getElementById("gradebook");
+		data.forEach(function(assignment){
+			let row = document.createElement("tr")
+			let columns = [];
+			columns.name = document.createElement('td');
+			columns.name.appendChild(
+				document.createTextNode(assignment.last_name + ", " + assignment.first_name)
+			);
+			columns.grade = document.createElement('td');
+			columns.grade.appendChild(
+				
 
-	data.forEach(function (assignment) {
-		let row = document.createElement("tr");
-
-		let nameCell = document.createElement("td");
-		nameCell.appendChild(
-			document.createTextNode(`${assignment.last_name}, ${assignment.first_name}`)
-		);
-
-		let gradeCell = document.createElement("td");
-		gradeCell.appendChild(
-			document.createTextNode(assignment.total_grade)
-		);
-
-		row.appendChild(nameCell);
-		row.appendChild(gradeCell);
-
-		tableElm.appendChild(row);
-	});
+				document.createTextNode(assignment.total_grade)
+			);
+			
+			row.appendChild(columns.name);
+			row.appendChild(columns.grade);
+			
+			tableElm.appendChild(row);
+		});
 }
 
-fetchGradeData();
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
