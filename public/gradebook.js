2b7981b2c0fd82cda4f2e8ab10af961071483dd9
Edward3785 <emichael1@my.waketech.edu>
2025-05-02 19:01:55 -0400

    moved status up

--- public/gradebook.js
+++ public/gradebook.js
@@ -9,8 +9,7 @@ function fetchGradeData() {
 		let results;
 		if(xhr.readyState === xhr.DONE){
 			if(xhr.status !== 200){
-				console.error('Could not get grades.
-					Status: $(xhr.status}');
+				console.error('Could not get grades.Status: $(xhr.status}');
 			}
 			populateGradebook(JSON.parse(xhr.responseText));
 			}
