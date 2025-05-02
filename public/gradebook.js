46ea45e0d4c3ddca010530b44d1b9a49b9fc82b9
Edward3785 <emichael1@my.waketech.edu>
2025-05-02 19:20:50 -0400

    revert

--- public/gradebook.js
+++ public/gradebook.js
@@ -9,7 +9,8 @@ function fetchGradeData() {
 		let results;
 		if(xhr.readyState === xhr.DONE){
 			if(xhr.status !== 200){
-				console.error('Could not get grades. Status: ${xhr.status}');
+				console.error('Could not get grades.
+					Status: $(xhr.status}');
 			}
 			populateGradebook(JSON.parse(xhr.responseText));
 			}
@@ -24,7 +25,7 @@ function populateGradebook(data) {
 	let tableElm = document.getElementById("gradebook");
 		data.forEach(function(assignment){
 			let row = document.createElement("tr")
-			let columns = {};
+			let columns = [];
 			columns.name = document.createElement('td');
 			columns.name.appendChild(
 				document.createTextNode(assignment.last_name + ", " + assignment.first_name)
@@ -44,4 +45,5 @@ function populateGradebook(data) {
 }
 
 
-fetchGradeData();
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
