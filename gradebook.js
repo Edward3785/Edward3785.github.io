32706b3ac38b3043b9e243e5ba942d254e54f2f3
Edward3785 <emichael1@my.waketech.edu>
2025-05-02 15:11:38 -0400

    Gradebook Finished

---
+++ gradebook.js
@@ -0,0 +1,12 @@
+function fetchGradeData() {
+	console.log("Fetching grade data. . .");
+}
+
+
+function populateGradebook(data) {
+	console.log("Populating gradebook with data:", data);
+}
+
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
